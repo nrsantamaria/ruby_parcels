49ce50f05d297d247bf4d6b52278cb0cb8210337
Aubrey Masten and Nicole Santamaria <nicole@>
2017-04-24 11:01:09 -0700

    add code to pass initialize method spec, creating instance variable of dimensions data

--- lib/parcels.rb
+++ lib/parcels.rb
@@ -1,3 +1,10 @@
 class Parcel
-
+  define_method(:initialize) do |length, width, height, weight|
+    @details = {
+      "length" => length.to_f,
+      "width" => width.to_f,
+      "height" => height.to_f,
+      "weight" => weight.to_f
+    }
+  end
 end
--- spec/parcels_spec.rb
+++ spec/parcels_spec.rb
@@ -2,5 +2,7 @@ require('rspec')
 require('parcels')
 
 describe("Parcel#initialize") do
-
+  it('creates a new ofject of class Parcel') do
+    expect(Parcel.new(3,4,5,10).class).to(eq(Parcel))
+  end
 end
